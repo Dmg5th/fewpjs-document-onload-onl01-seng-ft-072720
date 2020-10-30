b795140cb9c29eb0e04000a42d4f3f18ec983f73
Dmg5th <dmgould310@gmail.com>
2020-10-30 10:11:24 -0400

    commit tree 4ea11c4b496e77b4400fcb149c38818369c52af8

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("The DOM has loaded");
+    updateDom();
   });
    
-  console.log(
-    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-  );
 
-  const element = document.getElementById("text"); 
-
-  element.innerHTML = "This is really cool!"
+function updateDom(){
+    document.getElementById("text").innerHTML= "This is really cool!"
+}
